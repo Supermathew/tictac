8326675469909f9cf29759b110a9eb126eae89be
Supermathew <dolikemathewalex@gmail.com>
2023-07-22 14:09:44 +0530

    requirement updated

--- tictac/asgi.py
+++ tictac/asgi.py
@@ -17,7 +17,7 @@ import os
 
 import home.routing
 from django.core.asgi import get_asgi_application
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -25,7 +25,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 # import appname.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tictac.settings')
-
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
