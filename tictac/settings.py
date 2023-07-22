fe74cbeaee2cf7e7a49db0d6a7bfffef59ea4555
Supermathew <dolikemathewalex@gmail.com>
2023-07-22 14:47:04 +0530

    requirement updatedm

--- tictac/settings.py
+++ tictac/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import django
+django.setup()
 from pathlib import Path
 import environ
 import os
